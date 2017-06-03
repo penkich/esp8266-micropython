06f59a7def5106803eb23608f6386b5f39325c59
Penkich <nishijim2001@yahoo.co.jp>
2017-06-03 12:15:51 +0900

    Create heart.py

---
+++ kinenhin/heart.py
@@ -0,0 +1,31 @@
+#
+# neopixcel test programn 2017-06-03
+# CC0:No writes reserved
+# ハートの鼓動をイメージして色を変化させる
+#
+
+from machine import Pin
+from neopixel import NeoPixel
+from time import *
+import urandom
+
+urandom.seed(2017)
+pin = Pin(4,Pin.OUT) # NeoPixel data port
+np = NeoPixel(pin, 5) # use 5 NeoPixels
+
+def test(pw, t, bia):
+    for i in range(pw):
+        np.fill([i, pw-i, bia])
+        np.write()
+        sleep(t)
+    for i in range(pw):
+        np.fill([pw-i, bia, i])
+        np.write()
+        sleep(t)
+    for i in range(pw):
+        np.fill([bia, i, pw-i])
+        np.write()
+        sleep(t)
+
+while True:
+    test(100,0.01,urandom.getrandbits(6))
